52551bb2476421653d7968be18ce927fe712cf50
twixtaxe <igormota999@gmail.com>
2022-03-01 20:56:26 -0300

    first commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,76 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Hash from '@ioc:Adonis/Core/Hash'
+import User from 'App/Models/User'
+import dataValidator from '../../../utils/validator/user'
+import render from '../../../utils/render/user'
+
+export default class AuthController {
+  public async register({ request, response, auth }: HttpContextContract) {
+    const { password } = request.body()
+    const data = await request.validate({ schema: dataValidator.userSchema })
+    const user = await User.create(data)
+
+    const token = await auth.attempt(user.email, password)
+
+    return response.status(201).send({
+      message: 'Usuario criado com sucesso',
+      data: render.user(user),
+      token,
+    })
+  }
+
+  public async login({ request, response, auth }: HttpContextContract) {
+    const { email, password } = request.only(['email', 'password'])
+
+    const query = await Database.from('users').select('*').where('email', email)
+    const user = query[0]
+
+    if (!user) {
+      return response.status(404).send({ Message: 'User not found' })
+    }
+
+    if (!(await Hash.verify(user.password, password))) {
+      return response.status(404).send({
+        message: 'Unauthorized',
+      })
+    }
+    const token = await auth.attempt(email, password)
+
+    return response.status(200).send({
+      Message: 'Authorized',
+      email: email,
+      data: render.user(user),
+      token,
+    })
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+    await user.delete()
+    return response.status(240).send({
+      messsage: 'user deleted',
+    })
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const data = await request.validate({ schema: dataValidator.userSchema })
+    const user = await User.findOrFail(params.id)
+
+    user.username = data.username
+    user.email = data.email
+
+    await user.save()
+
+    return response.status(200).send({
+      message: 'user updated',
+      user: render.user(user),
+    })
+  }
+
+  public async teste() {
+    return {
+      messae: 'teste',
+    }
+  }
+}
---
+++ app/Middleware/Auth.ts
@@ -0,0 +1,25 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class Auth {
+  public async handle({ request, response, auth }: HttpContextContract, next: () => Promise<void>) {
+    // code for middleware goes here. ABOVE THE NEXT CALL
+    const { authorization } = request.headers()
+
+    if (!authorization) {
+      return response.status(404).send({
+        message: 'not token',
+      })
+    }
+    if (!authorization.includes('Bearer ')) {
+      return response.status(404).send({
+        messahe: 'token mal formated',
+      })
+    }
+    if (!(await auth.check())) {
+      return response.status(401).send({
+        message: 'token not valid',
+      })
+    }
+    await next()
+  }
+}
